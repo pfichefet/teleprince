08f1299e51114f24706b184384286db531cf02df
Saumil Gauswami <saumil@lapisinfo.com>
2023-09-06 18:33:54 +0530

    [ADD][16.0]ADded module to Add Payment Method On the Invoice

---
+++ add_payment_to_invoice/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Add Payment Method On the Invoice",
+
+    'summary': """
+   """,
+    'category': 'invoice',
+    'version': '16.0.1.0.0',
+
+    'depends': ['account'],
+
+    'data': [
+        'views/report_invoice.xml'
+    ],
+    'license': 'LGPL-3',
+}
