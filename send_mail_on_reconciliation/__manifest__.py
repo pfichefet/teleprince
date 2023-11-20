4ca6fddc1f1cb130d324bf88bcd2f877eb35e282
Ankita Raval <ankita.raval@ensigncode.com>
2023-11-20 16:00:06 +0530

    [ADD][16.0] Added new module for send email on reconciliation.

---
+++ send_mail_on_reconciliation/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Send Email Automatically on Reconciliation",
+
+	'summary': """
+   """,
+	'category': 'account',
+	'version': '16.0.0.0.0',
+
+	'depends': ['account'],
+
+	'data': [
+		'data/mail_template_data.xml',
+	],
+	'license': 'LGPL-3',
+}
