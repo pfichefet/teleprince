b52ef71d955ae6a356a112ec1e35dc94b5452270
Nisha Gajjar <nisha.gajjar@skyscendbs.com>
2024-01-10 18:04:50 +0530

    [IMP]production api fix

--- sale_inventory_api/models/sale_order.py
+++ sale_inventory_api/models/sale_order.py
@@ -17,7 +17,7 @@ class SaleOrder(models.Model):
 		:return:
 		"""
 		sale_line_list = []
-		sale_order_ids = self.search([('state', '=', 'sale'), ('invoice_ids', '!=', []), ('api_triggered', '=', False),('date_order','!=',False)])
+		sale_order_ids = self.search([('state', '=', 'sale'), ('invoice_ids', 'not in', []), ('api_triggered', '=', False), ('date_order', '!=', False)])
 		for sale_line in sale_order_ids.mapped('order_line').filtered('product_id'):
 			order_id = sale_line.order_id
 			year, month, day, hour, minute, second = order_id.date_order.timetuple()[:6]
@@ -28,11 +28,11 @@ class SaleOrder(models.Model):
 				"salesDate": f"{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}.0000Z",
 				"salesReference": order_id.name,
 				"lineNo": sale_line.id,
-				"productDescription": sale_line.product_id.name,
+				"productDescription": sale_line.product_id.name[:100],
 				"storeName": order_id.company_id.name,
 			})
-			if not order_id.api_triggered:
-				order_id.api_triggered = True
+   # if not order_id.api_triggered:
+   # 	order_id.api_triggered = True
 		if sale_line_list:
 			post_sale_inventory_api = PostSaleInventory(self.env.company.b_and_o_api_key, self.env.company.b_and_o_api_environment)
 			return post_sale_inventory_api.post_sale_data(sale_line_list)
