1a7283b05b68d00613f302ffc2071192891ac3ef
Nisha Gajjar <nisha.gajjar@skyscendbs.com>
2024-07-19 20:54:59 +0530

    [IMP] trigger update

--- sale_inventory_api/models/stock_quant.py
+++ sale_inventory_api/models/stock_quant.py
@@ -52,7 +52,7 @@ class StockQuant(models.Model):
                 if quant.lot_id:
                     quant_list.append({
                         "storeId": company_rec.b_and_o_store_id,
-                        "productNo": quant.product_id and quant.product_id.default_code and int(quant.product_id.default_code) or quant.product_id.id,
+                        "productNo": quant.product_id.id,
                         "onhandQuantity": quant.quantity > 0 and quant.quantity or 0,
                         "inventoryDate": f"{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}.0000Z",
                         "productDescription": quant.product_id.name[:100],
@@ -64,7 +64,7 @@ class StockQuant(models.Model):
                 else:
                     quant_list.append({
                         "storeId": company_rec.b_and_o_store_id,
-                        "productNo": quant.product_id and quant.product_id.default_code and int(quant.product_id.default_code) or quant.product_id.id,
+                        "productNo": quant.product_id.id,
                         "onhandQuantity": quant.quantity > 0 and quant.quantity or 0,
                         "inventoryDate": f"{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}.0000Z",
                         "productDescription": quant.product_id.name[:100],
