6ddaaa0b0136132e15e7488be360b7d3944c9ab9
Pierrick Fichefet <pfi@idealisconsulting.com>
2025-01-11 16:00:25 +0100

    [FIX] T-91: When an intervention task is marked as closed we generate a sale order to invoice the timesheets.

--- crm_project_task/models/project_task.py
+++ crm_project_task/models/project_task.py
@@ -13,9 +13,10 @@ class ProjectTask(models.Model):
         Override method.
         The opportunity linked to the task move into the pre-visit done stage.
         """
-        super().action_fsm_validate(stop_running_timers=stop_running_timers)
+        res = super().action_fsm_validate(stop_running_timers=stop_running_timers)
         for task in self:
             if task.is_pre_visit_task:
                 pre_visit_done_crm_stage = self.env["crm.stage"].search([("is_pre_visit_done_stage", "=", True)], limit=1)
                 if pre_visit_done_crm_stage and self.opportunity_id:
                     self.opportunity_id.stage_id = pre_visit_done_crm_stage
+        return res
