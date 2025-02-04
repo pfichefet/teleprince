d254fc420531a3adf1f4f593a2c499c2bdb61f63
Pierrick Fichefet <pfi@idealisconsulting.com>
2025-02-04 12:11:09 +0100

    [IMP] T_104: Don't group PO during MTO + display customer on PO

---
+++ sale_purchase_addons/models/purchase_order.py
@@ -0,0 +1,38 @@
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    sale_partner_id  = fields.Many2one('res.partner', string="Customer", compute='_compute_sales_partner_id', search="_search_sale_partner_id", help="Customer of the main sale order")
+
+    def _compute_sales_partner_id(self):
+        """
+        Get the partner of the first sale order
+        """
+        for po in self:
+            sale_orders = po._get_sale_orders()
+            if sale_orders:
+                po.sale_partner_id = sale_orders[0].partner_id
+            else:
+                po.sale_partner_id = False
+
+    def _search_sale_partner_id(self, operator, value):
+        """
+        Search method to enable searching on the computed field sale_partner_id
+        """
+        if not value:
+            # Handle the 'is set' and 'is not set' filters
+            sale_domain = []
+            final_operator = 'not in' if operator == '=' else 'in'
+        else:
+            # Handle other filters
+            list_partners_name = self.env['res.partner'].search_read(
+                [('name', operator, value)], ['name']
+            )
+            partner_ids = [partner_name['id'] for partner_name in list_partners_name]
+            sale_domain = [('partner_id', 'in', partner_ids)]
+            final_operator = '='
+        sale_orders = self.env['sale.order'].search(sale_domain)
+        purchase_order_ids = sale_orders._get_purchase_orders().ids
+        return [('id', final_operator, purchase_order_ids)]
