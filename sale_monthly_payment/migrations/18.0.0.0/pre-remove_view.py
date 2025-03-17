bb13865ef488cc7435e96ff0e5084fde09144691
Pierrick Fichefet <pfi@idealisconsulting.com>
2025-03-17 12:07:22 +0100

    [MIG-v18] T_79: Migration scrip remove old views

--- sale_monthly_payment/migrations/18.0.0.0/pre-remove_view.py
+++ sale_monthly_payment/migrations/18.0.0.0/pre-remove_view.py
@@ -4,5 +4,5 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    cr.execute("DELETE FROM ir_ui_view WHERE arch_prev LIKE 'line_taxed_total'")
+    cr.execute("DELETE FROM ir_ui_view WHERE arch_prev LIKE '%line_taxed_total%'")
     _logger.info("Remove %s views with line_taxed_total", cr.rowcount)
