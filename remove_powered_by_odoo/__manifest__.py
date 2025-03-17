ea3b8e82e91553b7a731d7c80d137328b3b71a0f
Pierrick Fichefet <pfi@idealisconsulting.com>
2025-03-17 11:13:59 +0100

    [MIG-v18] T_79: Add empty module to remove warning during update

---
+++ remove_powered_by_odoo/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Remove Powered By Odoo",
+    "summary": """
+   """,
+    "category": "mail",
+    "version": "18.0.1.0.0",
+    "depends": [],
+    "data": [],
+    "license": "LGPL-3",
+}
