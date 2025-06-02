019557ae6eb9e0586466daa3df8459c5f44e5499
Pierrick Fichefet <pfi@idealisconsulting.com>
2025-06-02 17:00:01 +0200

    [IMP] T-680: Allow to assign an invoicing address and a delivery address on a field service task

--- project_task_addons/models/project_task.py
+++ project_task_addons/models/project_task.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.osv import expression
 from odoo.tools.misc import unquote
 
@@ -50,3 +50,27 @@ class Task(models.Model):
     project_partner_id = fields.Many2one(
         "res.partner", string="Project Partner", related="project_id.partner_id"
     )
+    delivery_partner_id = fields.Many2one(
+        "res.partner",
+        string="Delivery Partner",
+        compute="_compute_delivery_partner",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends("partner_id")
+    def _compute_delivery_partner(self):
+        """
+        Partner (address) at which the physical operation take place.
+        Might be different from the invoicing address.
+        The standard field 'partner_id' is used to select the partner that must be invoiced.
+        """
+        for task in self:
+            if task.partner_id and not task.delivery_partner_id:
+                task.delivery_partner_id = task.partner_id
+
+    def _get_partner_emails(self):
+        """Get comma-separated attendee email addresses."""
+        self.ensure_one()
+        partners = self.partner_id | self.delivery_partner_id
+        return ",".join([e for e in partners.mapped("email") if e])
